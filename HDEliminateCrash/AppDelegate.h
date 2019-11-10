c845ea12ff8c70e42c975b6b0d1fe3f5a787df36
hdong <809184372@qq.com>
2019-11-10 15:29:15 +0800

    cocoaPodsTest

---
+++ HDEliminateCrash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HDEliminateCrash
+//
+//  Created by 黄东 on 2019/10/21.
+//  Copyright © 2019 黄东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HDEliminateCrash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HDEliminateCrash
+//
+//  Created by 黄东 on 2019/10/21.
+//  Copyright © 2019 黄东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
